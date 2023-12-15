d544ca6eacc651e3ce7171e9e614bea50e9f957e
kouta <darkfm@vera.com.uy>
2023-12-15 16:10:39 -0300

    Fix option replace order (prioritize customized options)

--- Adapters/Curl.php
+++ Adapters/Curl.php
@@ -70,7 +70,7 @@ class Curl implements IAdapter
         ];
 
         if (is_array($this->options) && !empty($this->options)) {
-            $options = array_replace($this->options, $options);
+            $options = array_replace($options, $this->options);
         }
 
         if (!empty($params)) {
